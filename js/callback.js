b3ba3d299217d658969982299e1ebda2d64ff79e
gerardo <gerardoevq@gmail.com>
2025-02-13 12:24:01 -0700

    funciones predefinidas para texto

---
+++ js/callback.js
@@ -0,0 +1,20 @@
+
+/*let segundonumero=5
+function sumar(primernumero) {
+    let segundonumero=15;
+    console.log(primernumero+segundonumero) 
+}
+sumar(10)
+console.log()*/
+
+//suma(5,4,9)
+function suma(numero1,numero2,numero3) {
+    let resultado=numero1+numero2+numero3
+    return resultado
+}
+
+multiplicacion(5,suma(5,4,9))
+function multiplicacion(numero1,numero2) {
+    let resultado=numero1*numero2
+        console.log(resultado)
+}

f8119e0654d2416410aec46f8745c5fa7c24888f
gerardo <gerardoevq@gmail.com>
2025-02-24 17:16:20 -0700

    practicas  clases y callbacks

--- js/callback.js
+++ js/callback.js
@@ -8,7 +8,7 @@ sumar(10)
 console.log()*/
 
 //suma(5,4,9)
-function suma(numero1,numero2,numero3) {
+/*function suma(numero1,numero2,numero3) {
     let resultado=numero1+numero2+numero3
     return resultado
 }
@@ -17,4 +17,92 @@ multiplicacion(5,suma(5,4,9))
 function multiplicacion(numero1,numero2) {
     let resultado=numero1*numero2
         console.log(resultado)
+}*/
+//ejercicio de operacion sincrona
+/*function texto() {
+    console.log("soy un callback")
 }
+function operacion(params) {
+    params()
+}
+operacion(texto)*/
+
+/*function fun1(callback) {
+    console.log("inicio")
+    callback()
+
+    console.log("fin")
+}
+function fun2() {
+    console.log("proceso intermedio")
+}
+fun1(fun2)*/
+/*function comprar(art1,art2,callback) {
+    let mensaje=art1+art2
+    callback(mensaje)
+}
+function imprimir(mensaje) {
+    console.log(`el monto gastado es de ${mensaje}`)
+}
+comprar(100,200,imprimir)*/
+/*function firstAction(callback) {
+    console.log("I'm the first action")
+    setTimeout(callback,5000)
+
+  }
+  function secondAction() {
+    console.log("I'm the second action")
+  }
+  setTimeout(()=>{
+    firstAction(secondAction)
+    },3000);*/
+/*function primero() {
+    console.log("primero")
+}
+function segundo() {
+    console.log("SEGUNDO")
+}
+primero()
+segundo()*/
+//este es el problema con la asincronia(retraso de envio de informacion)
+/*function primero() {
+    setTimeout(() => {
+        console.log("primero") 
+    }, 3000);
+}
+function segundo() {
+    console.log("SEGUNDO")
+}
+primero()
+segundo()*/
+// asi soluciono el problema de la asincronia(usando callbacks)
+/*function primero(segundo) {
+    setTimeout(() => {
+        console.log("primero") 
+        segundo()
+    }, 3000);
+}
+function segundo() {
+    console.log("SEGUNDO")
+}
+primero(segundo)*/
+/*function suma(uno,dos) {
+    console.log(uno+dos())
+}
+function resta() {
+    return 5-4
+}
+suma(5,resta)*/
+/*
+function inicio(uno,dos) {
+    dos(uno)    
+}
+inicio("gerardo",function (uno) {
+    console.log(`bienvenido ${uno}`)
+})*/
+function suma(articulo1,artuculo2,suma) {
+    let suma=articulo1+artuculo2
+}
+suma(5,10,()=>{
+    console.log(`el total es ${suma}`)
+})
--- js/clases.js
+++ js/clases.js
@@ -30,7 +30,7 @@ console.log(persona)
 console.log(perro.genero)
 perro.saludar()
 gato.saludar()*/
-
+/*
 class Animal {
     constructor(nombre,genero) {
         this.nombre=nombre;
@@ -46,12 +46,40 @@ class Perro extends Animal {
         super(nombre,genero);
         this.tamano=tamano;
     }
-    sonido(){
+    sonido(){ // esto es un metodo, es una funcion que esta dentro de una clase
         console.log(`soy un perro y mi sonido es un ladrido`)
     }
 
 }
-const betoben = new Perro("gordo","macho","mediano");
+const betoben = new Perro("gordo","macho","mediano"); // esto se llama instanciar(crear un objeto utilizando una clase)
 console.log(betoben.nombre)
 betoben.sonido()
-betoben.saludar()
+betoben.saludar()*/
+
+class Animal {
+    constructor(nombre,genero) {
+        this.nombre=nombre;
+        this.genero=genero;
+    }
+    sonar(){
+        console.log("hago sonidos")
+    }
+    saludar(){
+        console.log(`hola me llamo ${this.nombre}`)
+    }
+}
+class Perro extends Animal {
+    constructor(nombre,genero,tamano) {
+        super(nombre,genero);
+        this.tamano=tamano;
+    }
+    sonar(){// aqui estoy sobreescribiendo el metodo de la clase Animal
+        console.log("hola mundo")
+    }
+    static hermosillo(Perro){//los metodos estatico necesitan llevar la palabra static y no es necesario utilizar una instancia(new) parautilizarlos
+        console.log(`me llamo ${Perro.nombre}`)
+    }
+}
+const gordo = new Perro("gordo","macho","mediano");
+gordo.sonar()
+Perro.hermosillo(gordo)
